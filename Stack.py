ce6b42b5444e1c1b6277602511f9ddb001df349a
pie <returnpie@icloud.com>
2020-10-22 22:12:36 +0900

    stack

---
+++ Stack.py
@@ -0,0 +1,49 @@
+# LIFO: last item insert is the first item we take out
+
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    # insert item into the stack
+    def push(self, data):
+        self.stack.append(data)
+
+    # remove and return the last item we have inserted (LIFO)
+    def pop(self):
+
+        if self.stack_size() < 1:
+            return -1
+
+        data = self.stack[-1]
+        del self.stack[-1]
+        return data
+
+    # it returns the last item without removing it
+    def peek(self):
+
+        if self.stack_size() < 1:
+            return -1
+
+        return self.stack[-1]
+
+    # has 0(1) running time
+    def is_empty(self):
+        return self.stack == []
+
+    def stack_size(self):
+        return len(self.stack)
+
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print('Size: %d' % stack.stack_size())
+print('Popped item: %d' % stack.pop())
+print('Size: %d' % stack.stack_size())
+print('Peeked item: %d' % stack.peek())
+print('Size: %d' % stack.stack_size())
+print('Popped item: %d' % stack.pop())
+print('Size: %d' % stack.stack_size())
